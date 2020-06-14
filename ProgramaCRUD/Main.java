79114227e0f7cdf0aed13f1d92ca04b0ff1131b0
Wesley Felix <wesleyfelix.m@gmail.com>
2020-06-14 17:43:31 -0300

    Pacotes

--- ProgramaCRUD/Main.java
+++ ProgramaCRUD/Main.java
@@ -1,12 +1,8 @@
 package ProgramaCRUD;
 
-import javax.swing.*;
-
 public class Main {
     public static void main(String Args[]){
-        JOptionPane.showMessageDialog(null, "Olá, Mundo!");
 
-        System.out.println("Olá, mundo!");
     }
-    
+
 }
---
+++ ProgramaCRUD/view/TelaPedido.java
@@ -0,0 +1,4 @@
+package ProgramaCRUD.view;
+
+public class TelaPedido {
+}
