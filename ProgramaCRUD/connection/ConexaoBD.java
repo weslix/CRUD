6ae484d80b6c46b9d1b195f2f994629d6f5c913f
Wesley Felix <wesleyfelix.m@gmail.com>
2020-06-14 17:47:06 -0300

    Branch connection

--- ProgramaCRUD/connection/ConexaoBD.java
+++ ProgramaCRUD/connection/ConexaoBD.java
@@ -1,1 +1,31 @@
+package ProgramaCRUD.connection;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoBD {
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Connection conectar() throws SQLException{
+        String servidor = "localhost";
+        String porta = "3306";
+        String database = "cadastro_cliente";
+        String usuario = "root";
+        String senha = "123mudou";
+        return DriverManager.getConnection("jdbc:mysql://"+servidor+":"+porta+"/"+database+"?useTimezone=true&serverTimezone=UTC",usuario,senha);
+    }
+
+    public static void desconectar(@NotNull Connection conn) throws SQLException{
+        conn.close();
+    }
+}
